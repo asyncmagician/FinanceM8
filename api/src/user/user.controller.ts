338581293d1d0cf3ef1d6425628f3c0a8d89e4aa
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-04-22 19:23:20 +0200

    feat: Set ResponseHeader and catch error with findAll

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,14 +1,30 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, Res, HttpStatus, HttpException } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { ResponseHeader } from '../interfaces/responseHeader';
+import { Response } from 'express';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
-  async findAll(): Promise<User[]> {
-    return await this.userService.findAll();
+  async findAll(@Res() res: Response): Promise<void> {
+    try {
+      const users = await this.userService.findAll();
+
+      const header = new ResponseHeader('success', '/users', 'The operation was successful.', 200);
+
+      res.status(200).json({ header, body: users });
+    } catch (error) {
+      const header = new ResponseHeader('error', '/users', error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+
+      if (error.status === HttpStatus.NOT_FOUND) {
+        throw new HttpException({ header, body: [] }, HttpStatus.NOT_FOUND);
+      } else {
+        throw new HttpException({ header, body: [] }, HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    }
   }
 
   @Get(':id')
