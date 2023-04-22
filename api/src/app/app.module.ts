fe331cbe1dfa9cd1968dcd819c3bdc66941c67ce
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-04-22 15:51:37 +0200

    chore: Update main application + module

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from '../config/typeorm.config';
+// import { UserModule } from '../user/user.module';
+// import { BalanceModule } from '../balance/balance.module';
+// import { ExpenseModule } from '../expense/expense.module';
+// import { ExpenseCategoryModule } from '../expense-category/expense-category.module';
+// import { RefundModule } from '../refund/refund.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { config } from 'dotenv';
-
-config();
+import { AppModule } from './app/app.module';
+import * as dotenv from 'dotenv';
+ 
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
