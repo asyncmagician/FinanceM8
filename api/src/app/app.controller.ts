575b7fb30d689be084468789eaef1564afc9eea9
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-04-22 19:21:53 +0200

    fix: Fix AppController to catch 404 error with the API

--- api/src/app/app.controller.ts
+++ api/src/app/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Res, Get, HttpStatus, NotFoundException } from '@nestjs/common';
+import { Response } from 'express';
+import { ResponseHeader } from '../interfaces/responseHeader';
 
 @Controller()
-export class AppController {}
+export class AppController {
+    @Get('*')
+    notFound(@Res() res: Response): void {
+      const header = new ResponseHeader('error', '*', 'Route not found', HttpStatus.NOT_FOUND);
+  
+      throw new NotFoundException({ header, body: [] });
+    }
+}
