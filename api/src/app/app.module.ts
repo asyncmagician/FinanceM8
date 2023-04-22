1b1e3eb3185f88bcb569bded0e1dc7570cbc1de2
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-04-22 15:54:17 +0200

    chore: Update AppModule to import UserModule

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from '../config/typeorm.config';
-// import { UserModule } from '../user/user.module';
+import { UserModule } from '../user/user.module';
 // import { BalanceModule } from '../balance/balance.module';
 // import { ExpenseModule } from '../expense/expense.module';
 // import { ExpenseCategoryModule } from '../expense-category/expense-category.module';
@@ -9,7 +9,7 @@ import { typeOrmConfig } from '../config/typeorm.config';
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule],
   controllers: [],
   providers: [],
 })
