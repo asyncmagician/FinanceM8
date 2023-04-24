1bf3bebd350c069a6099676e4c5d44e2fb465fc1
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-04-24 15:06:13 +0200

    chore: Update expenseCategory import due to recent refactor

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from '../config/typeorm.config';
 import { UserModule } from '../user/user.module';
 import { RefundModule } from '../refund/refund.module';
-import { ExpenseCategoryModule } from 'src/expenseCategory/expense-category.module';
+import { ExpenseCategoryModule } from 'src/expenseCategory/expenseCategory.module';
 import { ExpenseModule } from '../expense/expense.module';
 import { BalanceModule } from '../balance/balance.module';
 import { AuthModule } from '../auth/auth.module'
