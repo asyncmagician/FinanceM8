a4c9b2da15c6e8517c2fd59f32d1597eab703c72
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-04-24 15:02:17 +0200

    feat: Enable CORS for the GUI to fetch data

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import * as dotenv from 'dotenv';
+
  
 dotenv.config();
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(
+    {
+      origin: process.env.appUrl, 
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      allowedHeaders: 'Content-Type, Accept, Authorization',
+    }
+  );
   await app.listen(process.env.PORT);
 }
 bootstrap();
