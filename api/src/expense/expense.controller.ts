662d3b0a155b58a465ea70c352f62f5a9550daaa
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-04-24 15:04:58 +0200

    feat: Update JWT authentication for HTTP requests on expense

--- api/src/expense/expense.controller.ts
+++ api/src/expense/expense.controller.ts
@@ -1,32 +1,38 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, UseGuards } from '@nestjs/common';
 import { ExpenseService } from './expense.service';
 import { Expense } from './expense.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('expenses')
 export class ExpenseController {
   constructor(private expenseService: ExpenseService) {}
 
   @Get()
+  @UseGuards(AuthGuard('jwt'))
   async findAll(): Promise<Expense[]> {
     return await this.expenseService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
   async findOne(@Param('id') id: number): Promise<Expense> {
     return await this.expenseService.findOne(id);
   }
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
   async create(@Body() expense: Expense): Promise<Expense> {
     return await this.expenseService.create(expense);
   }
 
   @Put(':id')
+  @UseGuards(AuthGuard('jwt'))
   async update(@Param('id') id: number, @Body() expense: Expense): Promise<void> {
     await this.expenseService.update(id, expense);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('jwt'))
   async delete(@Param('id') id: number): Promise<void> {
     await this.expenseService.delete(id);
   }
