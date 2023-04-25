080ada1be246d91260b3f16ee3b0fc4ba52b01e0
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-04-25 15:57:15 +0200

    feat: Delete Astro.js GUI

--- app/src/lib/checkAuth.ts
+++
@@ -1,9 +0,0 @@
-export function isAuthenticated(): boolean {
-    if (typeof window === 'undefined') {
-      return false;
-    }
-  
-    const authState = localStorage.getItem('authState');
-    return !!authState;
-  }
-  
